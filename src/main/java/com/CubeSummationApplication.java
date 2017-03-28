78f3e188a8e397abac21c0a28424982de2793818
Harold Murcia <hl.murcia222@uniandes.edu.co>
2017-03-28 14:39:13 -0500

    Upload files support

---
+++ src/main/java/com/CubeSummationApplication.java
@@ -0,0 +1,27 @@
+package com;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.storage.StorageProperties;
+import com.storage.StorageService;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class CubeSummationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubeSummationApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+		};
+	}
+}
